07b2a4353e120af1afe273d6b070dac516241777
qw644591074 <644591074@qq.com>
2016-12-21 20:56:24 +0800

    Update popup.js

--- popup.js
+++ popup.js
@@ -1,12 +1,2 @@
-$(document).ready(function(){
-$.ajax({
-				dataType:"jsonp",
-				method:"post",
-				url:'http://wthrcdn.etouch.cn/weather_mini?city=%e5%b9%bf%e5%b7%9e',
-				success:function(result){
-				var html="";
-				html=result.data.city+"---"+result.data.forecast[0].low+"~"+result.data.forecast[0].high+"---"+result.data.forecast[0].type;
-				$("#weather").append(html);
-				}
-});
+$(document).ready(function(){alert(" Welcome to ouwen's blog ");
 });
